617b6eb6ae5361a603313c01ea65fa3c970c52e9
H1rono <hronok66@gmail.com>
2025-07-15 17:18:44 +0900

    :construction: Add `Repository::within_tx`

--- repository/src/lib.rs
+++ repository/src/lib.rs
@@ -17,6 +17,33 @@ impl Repository {
             .context("Failed to run migrations")?;
         Ok(Self { pool })
     }
+
+    async fn within_tx<F, T, E>(&self, f: F) -> Result<T, E>
+    where
+        F: AsyncFnOnce(&mut sqlx::PgConnection) -> Result<T, E>,
+        E: crate::Error,
+    {
+        use anyhow::Context;
+
+        let mut tx = self
+            .pool
+            .begin()
+            .await
+            .context("Failed to begin postgres transaction")?;
+        let result = f(&mut tx).await;
+        match result {
+            Ok(v) => {
+                tx.commit().await.context("Failed to commit transaction")?;
+                Ok(v)
+            }
+            Err(e) => {
+                tx.rollback()
+                    .await
+                    .context("Failed to rollback transaction")?;
+                Err(e)
+            }
+        }
+    }
 }
 
 pub trait Error: domain::Error + From<anyhow::Error> {
